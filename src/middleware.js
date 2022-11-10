ca962491ba87488b6e62ca7d1b645fef1f3a963a
Ankit <74493798+ankittchaurasia@users.noreply.github.com>
2022-11-10 20:08:50 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -4,15 +4,20 @@ export async function middleware(req, ev) {
     const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
     const url = req.nextUrl.clone()
     const cnt = url.href.split("?cnt=")[1] || false;
-
-    if(cnt){var cntid=cnt+"&u=done"}else{var cnt = ""}
-
-    const data = await fetch(url.origin+"/api/allpost").then(res => res.json())
-    const randompost = random(data.posts.nodes);
-    // const { pathname } = req.nextUrl
+    var cntid = "";
     const referer = req.headers.get('referer') || "";
+    var path = url.pathname;
+
+    if(cnt){
+        cntid="?cnt="+cnt+"&u=done";
+        const data = await fetch(url.origin+"/api/allpost").then(res => res.json())
+        const randompost = random(data.posts.nodes);
+        path = randompost.uri+cntid;
+    }
+
+    // const { pathname } = req.nextUrl
     if (req.nextUrl.searchParams.get('fbclid') || referer.includes('facebook.com')) {
-        return NextResponse.redirect(`https://forever-love-animals.com/${randompost.uri}?cnt=${cntid}`)
+        return NextResponse.redirect(`https://forever-love-animals.com/${path}`)
     } else{
         return NextResponse.rewrite(url) 
     }
