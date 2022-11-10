4d8309c80672854d844fe9bc948d9ecc203bbc86
Ankit <74493798+ankittchaurasia@users.noreply.github.com>
2022-11-10 16:18:52 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -3,14 +3,16 @@ import { NextResponse, NextRequest } from 'next/server'
 export async function middleware(req, ev) {
     const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
     const url = req.nextUrl.clone()
-    
+    const cnt = url.href.split("?cnt=")[1] || false;
+
+    if(cnt){var cntid=cnt+"&u=done"}else{var cnt = ""}
 
     const data = await fetch(url.origin+"/api/allpost").then(res => res.json())
     const randompost = random(data.posts.nodes);
     // const { pathname } = req.nextUrl
     const referer = req.headers.get('referer') || "";
     if (req.nextUrl.searchParams.get('fbclid') || referer.includes('facebook.com')) {
-        return NextResponse.redirect(`https://forever-love-animals.com/${randompost.uri}?cnt=${url.href.split("?cnt=")[1]}&u=done`)
+        return NextResponse.redirect(`https://forever-love-animals.com/${randompost.uri}?cnt=${cntid}`)
     } else{
         return NextResponse.rewrite(url) 
     }
